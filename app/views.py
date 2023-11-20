4d5eef656c3b4d8481e0415c256f36b9380e3357
Chinnupinky10 <thummalapravallika4@gmail.>
2023-11-20 22:33:26 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def srujana(request):
+    return render(request,'srujana.html')
+
+def sudeer(request):
+    return render(request,'sudeer.html')
+
+def sudeer2(request):
+    return render(request,'sudeer2.html')
